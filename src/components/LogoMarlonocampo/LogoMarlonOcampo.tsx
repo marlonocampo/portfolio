3e2e2fa7ef7dec77a635a4bd09f5837334196d23
marlonocampo <marlonocampos@gmail.com>
2023-09-24 08:29:55 -0600

    agregando logo

---
+++ src/components/LogoMarlonocampo/LogoMarlonOcampo.tsx
@@ -0,0 +1,24 @@
+
+const OriginalLogo = (): JSX.Element => {
+  return (
+    <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 105 105'>
+      <g transform='translate(-709 -301.074)'>
+        <g transform='translate(714 259)'>
+          <g transform='translate(-5 42.074)'>
+            <rect width='27' height='99' rx='13.5' fill='currentcolor' />
+            <g transform='translate(72.775)'>
+              <rect width='26' height='69' rx='13' transform='translate(0.225)' fill='currentcolor' />
+              <circle cx='13' cy='13' r='13' transform='translate(0.225 73)' fill='currentcolor' />
+            </g>
+            <line
+              y2='25' transform='translate(48.944 32.5)'
+              fill='none' stroke='currentcolor' strokeLinecap='round' strokeWidth='19'
+            />
+          </g>
+        </g>
+      </g>
+    </svg>
+  )
+}
+
+export { OriginalLogo }
