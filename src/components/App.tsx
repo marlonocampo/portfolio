44b4b1653f422b3f62eee85f08ca8979c55bde75
marlonocampo <ocamposmarlon@gmail.com>
2023-10-24 07:33:55 -0600

    agregando favicon

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -10,7 +10,7 @@ export function App (): JSX.Element {
 
   return (
     <main data-theme={`${isDark ? 'darkTheme' : 'ligthTheme'}`} className='bg-primary scrollbarDark'>
-      <div className='badge text-black z-20 bg-amber-500 right-1 fixed mt-20 p-3 rounded-lg'>In</div>
+      <div className='badge text-black z-20 bg-amber-500 right-1 fixed mt-20 p-3 rounded-lg'>In Develop</div>
       <Navigation />
       <Presentation />
       <Skills />
