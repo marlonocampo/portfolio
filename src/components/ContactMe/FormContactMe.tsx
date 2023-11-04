96ba189f521076ed5287e99470a787f53e498bb8
marlonocampo <ocamposmarlon@gmail.com>
2023-11-04 02:01:10 -0600

    Agregando accecibilidad en formulario

--- src/components/ContactMe/FormContactMe.tsx
+++ src/components/ContactMe/FormContactMe.tsx
@@ -7,22 +7,23 @@ export function FormContactMe (): JSX.Element {
   return (
     <form onSubmit={sendEmail} className='text-secondary w-80 rounded-3xl border-secondary-focus shadow-xl border p-7'>
       <div className='form-control mb-4'>
-        <label className='label'>
+        <label className='label' for='name'>
           ¿Cuál es tu nombre?
         </label>
-        <input type='text' maxLength={50} className='input input-bordered input-secondary rounded-full  input-md w-full' />
+        <input type='text' id='name' maxLength={50} className='input input-bordered input-secondary rounded-full  input-md w-full' />
       </div>
       <div className='form-control mb-4'>
-        <label className='label'>
+        <label className='label' for='email'>
           Tu correo
         </label>
-        <input name='email' type='email' maxLength={30} placeholder='' className='input input-bordered rounded-full input-secondary  input-md w-full' />
+        <input name='email' id='email' type='email' maxLength={30} placeholder='' className='input input-bordered rounded-full input-secondary  input-md w-full' />
       </div>
       <div className='form-control mb-5'>
-        <label className='label'>
+        <label className='label' for='detail'>
           Detalle
         </label>
         <textarea
+          id='detail'
           placeholder='Escribe tu petición, sugerencia o pregunta'
           className='input input-bordered input-secondary input-md w-full rounded-2xl min-h-[100px] max-h-[200px]'
           maxLength={200}
