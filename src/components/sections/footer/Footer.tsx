d950d1efac4e719c96ac6c08f580fb1e03213879
marlonocampo <ocamposmarlon@gmail.com>
2023-11-05 09:11:16 -0600

    update color footer

--- src/components/sections/footer/Footer.tsx
+++ src/components/sections/footer/Footer.tsx
@@ -2,7 +2,7 @@ import { LogoMarlonOcampo } from 'src/components/LogoMarlonocampo/LogoMarlonOcam
 
 export function Footer (): JSX.Element {
   return (
-    <footer className='footer footer-center p-10 bg-primary border-t shadow-xl rounded-lg border-t-secondary-focus text-primary-content'>
+    <footer className='footer footer-center p-10 bg-primary border-t shadow-2xl shadow-secondary rounded-lg border-t-secondary-focus text-secondary'>
       <aside>
         <span className='h-16 w-16 m-5'>
           <LogoMarlonOcampo />
