a6c658a31255ea2168390ca71d90fad12b08af7a
marlonocampo <ocamposmarlon@gmail.com>
2024-05-12 07:22:11 -0600

    Eliminando dependencias para testing con 'Jest'

--- src/test/main.test.tsx
+++
@@ -1,8 +0,0 @@
-import '@testing-library/jest-dom'
-import { render } from '@testing-library/react'
-import { App } from '../../src/components/App'
-
-test('Render App Component', () => {
-  render(<App />)
-  expect(true).toBeTruthy()
-})
