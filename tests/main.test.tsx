6268e3e7e077f702b853b93ac11b19ef142ff1ae
marlonocampo <ocamposmarlon@gmail.com>
2024-05-12 09:04:31 -0600

    workflow for testing app

--- tests/main.test.tsx
+++ tests/main.test.tsx
@@ -1,14 +1,16 @@
 import { describe, expect, test } from 'vitest'
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import { App } from '../src/components/App'
+import { Presentation } from '../src/components/sections/presentation/Presentation'
 
 describe('Test main component Proyect', () => {
-  test('1 test Sucesss 😏!', () => {
-    expect('Success!').toBe('Success!')
-  })
-
-  test("Render component 'App'", () => {
+  test("Should render component 'App'", () => {
     render(<App />)
     expect(true).toBeTruthy()
+  })
+
+  test("Should render component 'Presentation.tsx' with text 'Soy Marlon Ocampo!'", () => {
+    const { getAllByText } = render(<Presentation />)
+    expect(getAllByText('Soy Marlon Ocampo!')).toBeDefined()
   })
 })
