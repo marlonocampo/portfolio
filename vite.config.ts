a4bc8b57a5c8af9f81c9983606b237d533d67ecc
marlonocampo <ocamposmarlon@gmail.com>
2025-06-28 01:40:11 -0600

    update vite and tailwindcss

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsConfigPaths from 'vite-tsconfig-paths'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [
     tsConfigPaths(),
-    react()
+    react(),
+    tailwindcss()
   ],
   base: '/'
 })
