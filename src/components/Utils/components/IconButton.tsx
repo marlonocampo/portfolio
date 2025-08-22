7faa582493496463d65b2087ad8fc6e3544ee30a
marlonocampo <ocamposmarlon@gmail.com>
2025-08-21 20:20:04 -0600

    fix lint rules

--- src/components/Utils/components/IconButton.tsx
+++ src/components/Utils/components/IconButton.tsx
@@ -9,7 +9,7 @@ export function IconButton ({ className, icon, onClick, ariaLabel }: Props): JSX
   return (
     <button
       onClick={() => onClick()}
-      className={`btn shadow-none border-0 bg-transparent hover:bg-secondary/10 btn-circle ${className}`} aria-label={ariaLabel}
+      className={`btn shadow-none border-0 bg-transparent hover:bg-secondary/10 btn-circle ${className ?? ''}`} aria-label={ariaLabel}
     >
       {icon}
     </button>
